c6559a9d82cd20cca0c6ad7ae2b5bbbc10f4256f
LucasMathW <lucasag0408@outlook.com>
2021-04-23 16:41:41 -0400

    generating episode page

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -4,7 +4,7 @@ export function Player(){
   return(
     <div className={styles.playerContainer}>
       <header>
-        <img src="playing.svg" alt="Tocando Agora"/>
+        <img src="/playing.svg" alt="Tocando Agora"/>
         <strong>Tocando agora</strong>
       </header>
       <div className={styles.emptyPlayer}>
